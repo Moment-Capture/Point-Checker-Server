f9b36fc895fb7865b7afc93f6c03d76cac3954eb
Joo Hyun Lee <michelle8085@gmail.com>
2024-04-12 14:52:19 +0900

    [home.py 변경/ex 삭제] 코드 정리 및 수정

--- build/home.py
+++ build/home.py
@@ -11,12 +11,136 @@ from reportlab.pdfgen import canvas
 from PIL import Image, ImageDraw, ImageFont
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\USER\Documents\Github\build\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"assets//assets//frame0")
+
+
+
+##########################################################
+##########################################################
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
+
+
+
+##########################################################
+##########################################################
+
 transfer_widgets = []
 home_widgets = []
+
+
+
+def process_input(num_candidates, file_path):
+    start_time = time.time()
+    insert(num_candidates, file_path)
+    end_time = time.time()  # 처리 종료 시간 기록
+    print(f"전체 처리 시간: {end_time - start_time} 초")
+    # show_grade()
+
+
+
+def insert(num_candidates, file_path):
+
+    output_pdf_path = os.path.splitext(file_path)[0] + "_pointchecker.pdf"
+
+    # PDF를 이미지로 변환하고 번호를 삽입하여 저장
+    images,num_pages = pdf_to_images(file_path)
+    processed_images = []
+    # 각 페이지에 대해 번호 삽입
+    for page_num in range(1, int(num_candidates) + 1):
+        for i in range(1, num_pages+1):
+            # 삽입할 숫자 (예: "1-1", "1-2", ...)
+            
+            number_to_insert = f"{page_num}-{i}"
+            # 삽입할 위치 (x, y 좌표)
+            insert_position = (100, 100)  # 적절한 위치로 수정 필요
+            
+            image = images[i-1].copy()
+            image = insert_number(image, number_to_insert, insert_position, i)
+            processed_images.append(image)
+    # 최종 PDF 파일 저장
+    save_as_pdf(output_pdf_path, num_candidates, processed_images)
+   
+
+
+def save_as_pdf(output_pdf_path, num_candidates, images):
+    # PDF 생성 및 이미지 추가
+    c = canvas.Canvas(output_pdf_path, pagesize=letter)
+
+    for image in images:
+
+        c.drawInlineImage(image, 0, 0, width=letter[0], height=letter[1])
+        c.showPage()
+    # PDF 파일 저장
+    c.save()
+
+
+
+def pdf_to_images(pdf_path):#output_folder):
+    images = []
+
+    # PDF 파일 열기
+    images = convert_from_path(pdf_path)
+    num_pages = len(images)
+
+    return images, num_pages
+
+
+
+def insert_number(image, text, position, page_num):
+
+    # 이미지에 숫자 삽입
+    draw = ImageDraw.Draw(image)
+    font_path = ImageFont.truetype("C:/Windows/Fonts/NanumGothic.ttf", 80)  # 폰트와 크기 설정
+    position = position  # 왼쪽 상단에 출력
+
+    # 삽입할 텍스트의 너비와 높이 초기화
+    total_width = 0
+    max_height = 0
+
+    # 하이픈을 포함한 텍스트 삽입
+    position_x, position_y = position
+    for char in text:
+
+      char_width = draw.textlength(char, font=font_path)
+      char_height = 80
+      draw.text((position_x, position_y), char, fill="black", font=font_path)
+      total_width += char_width
+      max_height = max(max_height, char_height)
+      position_x += char_width  # 다음 문자의 x 위치 조정
+    # 선 추가
+    if page_num==1:
+      underline_x1 = 1750 
+      underline_x2 = 2100 
+      underline_y = 200 
+      draw.line([(underline_x1, underline_y), (underline_x2, underline_y)], fill="black", width=2)
+
+        # 'id :' 텍스트 삽입
+      id_text = 'ID :'
+      id_position_x = 1600  # 박스 왼쪽에 위치하도록 조정
+      id_position_y = 130
+      draw.text((id_position_x, id_position_y), id_text, fill="black", font=font_path)
+
+    return (image)
+
+
+
+def hide_widgets(widget_list):
+    for widget in widget_list:
+        widget.place_forget()
+
+
+
+##########################################################
+##########################################################
+
+
+
+##########################################################
+##########################################################
+
+### 시험지 양식 적용 화면 ###
 def show_transfer():
     global home_widgets
     hide_widgets(home_widgets)
@@ -117,103 +241,15 @@ def show_transfer():
     global transfer_widgets
     transfer_widgets = [entry_2, button_2, button_1, file_path_label]
 
-def process_input(num_candidates, file_path):
-    start_time = time.time()
-    insert(num_candidates, file_path)
-    end_time = time.time()  # 처리 종료 시간 기록
-    print(f"전체 처리 시간: {end_time - start_time} 초")
-    show_home()
-
-def insert(num_candidates, file_path):
-
-    output_pdf_path = os.path.splitext(file_path)[0] + "_pointchecker.pdf"
-
-    # PDF를 이미지로 변환하고 번호를 삽입하여 저장
-    images,num_pages = pdf_to_images(file_path)
-    processed_images = []
-    # 각 페이지에 대해 번호 삽입
-    for page_num in range(1, int(num_candidates) + 1):
-        for i in range(1, num_pages+1):
-            # 삽입할 숫자 (예: "1-1", "1-2", ...)
-            
-            number_to_insert = f"{page_num}-{i}"
-            # 삽입할 위치 (x, y 좌표)
-            insert_position = (100, 100)  # 적절한 위치로 수정 필요
-            
-            image = images[i-1].copy()
-            image = insert_number(image, number_to_insert, insert_position, i)
-            processed_images.append(image)
-    # 최종 PDF 파일 저장
-    save_as_pdf(output_pdf_path, num_candidates, processed_images)
-   
-
-def save_as_pdf(output_pdf_path, num_candidates, images):
-    # PDF 생성 및 이미지 추가
-    c = canvas.Canvas(output_pdf_path, pagesize=letter)
-
-    for image in images:
-
-        c.drawInlineImage(image, 0, 0, width=letter[0], height=letter[1])
-        c.showPage()
-    # PDF 파일 저장
-    c.save()
 
 
-def pdf_to_images(pdf_path):#output_folder):
-    images = []
-
-    # PDF 파일 열기
-    images = convert_from_path(pdf_path)
-    num_pages = len(images)
-
-    return images, num_pages
-
-def insert_number(image, text, position, page_num):
-
-    # 이미지에 숫자 삽입
-    draw = ImageDraw.Draw(image)
-    font_path = ImageFont.truetype("C:/Windows/Fonts/NanumGothic.ttf", 80)  # 폰트와 크기 설정
-    position = position  # 왼쪽 상단에 출력
-
-    # 삽입할 텍스트의 너비와 높이 초기화
-    total_width = 0
-    max_height = 0
-
-    # 하이픈을 포함한 텍스트 삽입
-    position_x, position_y = position
-    for char in text:
-
-      char_width = draw.textlength(char, font=font_path)
-      char_height = 80
-      draw.text((position_x, position_y), char, fill="black", font=font_path)
-      total_width += char_width
-      max_height = max(max_height, char_height)
-      position_x += char_width  # 다음 문자의 x 위치 조정
-    # 선 추가
-    if page_num==1:
-      underline_x1 = 1750 
-      underline_x2 = 2100 
-      underline_y = 200 
-      draw.line([(underline_x1, underline_y), (underline_x2, underline_y)], fill="black", width=2)
-
-        # 'id :' 텍스트 삽입
-      id_text = 'ID :'
-      id_position_x = 1600  # 박스 왼쪽에 위치하도록 조정
-      id_position_y = 130
-      draw.text((id_position_x, id_position_y), id_text, fill="black", font=font_path)
-
-
-    return (image)
-    
-def hide_widgets(widget_list):
-    for widget in widget_list:
-        widget.place_forget()
-
-def show_home():
+### 채점하기 화면 ###
+def show_grade():
     global transfer_widgets
     hide_widgets(transfer_widgets)
     canvas_r.delete("all")  # 캔버스 초기화
-    #1. 시험정보를 입력하세요
+
+    # 1. 시험정보를 입력하세요
     canvas_r.create_text(
         40.0,
         20.0,
@@ -222,8 +258,8 @@ def show_home():
         fill="#000000",
         font=("Inter", 14 * -1)
     )
-    #시험명
 
+    # 1-1. 시험명
     canvas_r.create_text(
         40,
         50,
@@ -247,7 +283,7 @@ def show_home():
         height=23
     )
 
-    #시험지1부당매수
+    # 1-2. 시험지1부당매수
     canvas_r.create_text(
         40,
         80,
@@ -271,7 +307,7 @@ def show_home():
         height=23
     )
 
-    #총문항수
+    # 1-3. 총문항수
     canvas_r.create_text(
         40,
         110,
@@ -295,7 +331,7 @@ def show_home():
         height=23
     )
 
-    #응시자수
+    # 1-4. 응시자수
     canvas_r.create_text(
         40,
         140,
@@ -319,7 +355,7 @@ def show_home():
         height=23
     )
 
-    #문제유형
+    # 1-5. 문제유형
     canvas_r.create_text(
         40,
         170,
@@ -329,7 +365,7 @@ def show_home():
         font=("Inter", 14 * -1)
     )
 
-    # 객관식 체크박스 생성
+    # 1-6. 객관식 체크박스 생성
     choice_var = tk.IntVar()
     choice_checkbox = tk.Checkbutton(
         root,
@@ -345,7 +381,7 @@ def show_home():
         y=165
     )
  
-    # 단답식 체크박스 생성
+    # 1-7. 단답식 체크박스 생성
     short_answer_var = tk.IntVar()
     short_answer_checkbox = tk.Checkbutton(
         root,
@@ -361,7 +397,7 @@ def show_home():
         y=165
     )
 
-    #2. 채점할 시험지 파일 업로드하세요
+    # 2. 채점할 시험지 파일 업로드하세요
     canvas_r.create_text(
         40,
         220,
@@ -370,9 +406,9 @@ def show_home():
         fill="#000000",
         font=("Inter", 14 * -1)
     )
-    #시험지 파일 업로드 버튼
-    file_path_var=tk.StringVar()
 
+    # 2-1. 시험지 파일 업로드 버튼
+    file_path_var=tk.StringVar()
     file_path_label=tk.Label(
         bd=0,
         bg="#dddddd",
@@ -406,7 +442,7 @@ def show_home():
     )
 
 
-    #3.시험지 답을 입력해주세요
+    # 3. 시험지 답을 입력해주세요
     canvas_r.create_text(
         40,
         310,
@@ -414,7 +450,8 @@ def show_home():
         text="3. 시험지의 답을 입력해주세요.",
         fill="#000000",
         font=("Inter", 14 * -1)
-    ) 
+    )
+
     # 표의 행과 열 수 정의
     num_rows = 4
     num_columns = 15
@@ -427,6 +464,7 @@ def show_home():
     start_x = 200
     start_y = 340 
     cell_entries = []
+    
     # 표 생성
     for i in range(num_rows):
         for j in range(num_columns):
@@ -448,7 +486,7 @@ def show_home():
             
 
  
-    #4. 채점 버튼을 클릭하세요
+    # 4. 채점 버튼을 클릭하세요
     canvas_r.create_text( 
         40.0,
         490.0,
@@ -458,7 +496,7 @@ def show_home():
         font=("Inter", 14 * -1)
     )
 
-    #채점하기 버튼
+    # 4-1. 채점하기 버튼
     button_1 = tk.Button(
         text="채점하기",
         borderwidth=0,
@@ -473,11 +511,12 @@ def show_home():
         height=30
     )
 
-    # 진행 막대 생성
+    # 4-2. 진행 막대 생성
     progress_bar = ttk.Progressbar(root, orient="horizontal", length=260, mode="indeterminate")
     progress_bar.place(x=350, y=565)
 
-    #채점 결과 확인하기 버튼
+
+    # 4-3. 채점 결과 확인하기 버튼
     button_3 = tk.Button(
         text="채점 결과 확인하기",
         borderwidth=0,
@@ -491,12 +530,22 @@ def show_home():
         width=125,
         height=30
     )
-    # home_widgets 리스트 정의
+
+    ## home_widgets 리스트 정의 ##
     global home_widgets
     home_widgets = [entry_2,entry_3,entry_4,entry_5,choice_checkbox, short_answer_checkbox,button_3, button_2, button_1, file_path_label,progress_bar]
     home_widgets += [entry[0] for entry in cell_entries]
+    ## home_widgets 리스트 정의 ##
 
-    
+
+
+##########################################################
+##########################################################
+
+
+
+##########################################################
+##########################################################
 
 # 메인 창 생성
 root = tk.Tk()
@@ -526,11 +575,14 @@ canvas_r.pack(side=tk.RIGHT, padx=0, pady=0)
 # 툴바 버튼 생성 
 transfer_button = tk.Button(toolbar_frame, text="시험지 양식 적용", command=show_transfer, bg="#d9d9d9", font=("Inter", 14 * -1))
 transfer_button.pack(fill=tk.X, padx=10, pady=20,ipadx=10, ipady=20)
-home_button = tk.Button(toolbar_frame, text="채점 하기", command=show_home, bg="#d9d9d9",font=("Inter", 14 * -1)) 
+home_button = tk.Button(toolbar_frame, text="채점 하기", command=show_grade, bg="#d9d9d9",font=("Inter", 14 * -1)) 
 home_button.pack(fill=tk.X, padx=10, pady=10,ipadx=10, ipady=20) 
 
-# 초기 내용 표시
-show_home()
+# 메인 화면 설정
+show_grade()
 
 # 메인 루프 실행
 root.mainloop()
+
+##########################################################
+##########################################################
