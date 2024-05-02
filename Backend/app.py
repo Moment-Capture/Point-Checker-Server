d803895d4a4fc5cbf3eecafbbf288fb37c9dc85a
Joo Hyun Lee <michelle8085@gmail.com>
2024-05-02 17:05:27 +0900

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -53,17 +53,21 @@ def upload_pdf():
         answer_path = os.path.join(UPLOAD_FOLDER, answer_name)
         if answer and allowed_answer(answer_name):
             answer.save(answer_path)
+        
+        print("파일 업로드 성공")
 
-        # df = pd.DataFrame()
-        # df = getFinalDf(UPLOAD_FOLDER)
-        # json_data = df.to_json(orient="records")
-        # return json_data
+        df = pd.DataFrame()
+        df = getFinalDf(UPLOAD_FOLDER)
+        json_data = df.to_json(orient="records")
+        return json_data
+    
+    # return 200
         
 
 @app.route("/demo")
 def view_demo():
     df = pd.DataFrame()
-    df = getFinalDf()
+    df = getFinalDf(UPLOAD_FOLDER )
     json_data = df.to_json(orient="records")
     return json_data
 
