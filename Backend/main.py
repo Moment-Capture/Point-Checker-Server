7ef42dcd0eaa7c96229f47a5244283314c4240df
Joo Hyun Lee <112560299+gongkeo@users.noreply.github.com>
2024-05-13 19:57:26 +0900

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -68,6 +68,7 @@ def pointchecker(upload_path, test_name, copy_num, total_qna_num, testee_num, te
     testee_jpg_df = pd.DataFrame(columns=["file", "testee_id", "page"])
     id_match = {} #id_match는 testee_id와 id를 연결하는 딕셔너리
     testee_jpg_df, id_match = testeeCodeRecognition(jpg_file_path_list, testee_jpg_df)
+    testee_jpg_df.to_excel(path + "/testee_jpg_df.xlsx")
 
     # xlsx 파일 탐지
     answer_file_path_list = []
