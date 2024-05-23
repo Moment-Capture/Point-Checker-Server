7af3157f630a8cd966833f48b24deca186c0bff7
Joo Hyun Lee <michelle8085@gmail.com>
2024-05-23 19:30:05 +0900

    Update id.py

--- Backend/id.py
+++ Backend/id.py
@@ -7,6 +7,7 @@ import numpy as np
 from PIL import Image
 
 from utils import getNumText
+from utils import *
 
 sys.path.append(os.path.dirname(os.getcwd() + "/models/tamil_ocr/ocr_tamil"))
 from ocr_tamil.ocr import OCR
