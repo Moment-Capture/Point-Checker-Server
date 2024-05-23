eb82cd0350f58308c58f9a9cf8e0df2a326cd8e0
Joo Hyun Lee <112560299+gongkeo@users.noreply.github.com>
2024-05-23 13:36:01 +0900

    Update path.py

--- Frontend/path.py
+++ Frontend/path.py
@@ -1,8 +1,12 @@
+import os
 from pathlib import Path
 
 FILE_PATH = Path(__file__)
 OUTPUT_PATH = FILE_PATH.parent
-ASSETS_PATH = OUTPUT_PATH / "assets"
+
+DIR_PATH = Path(os.getcwd())
+
+ASSETS_PATH = DIR_PATH / "assets"
 FONT_PATH = "Malgun Gothic"
-# ICON_PATH = ASSETS_PATH / "pointchecker.ico"
 ICON_PATH = "pointchecker.ico"
+# ICON_PATH = ASSETS_PATH / "pointchecker.ico"
